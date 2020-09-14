55e13c1717b4b3dfd6e487298157a129a5369a88
NikitaKhadnevich <vadim-swich@yandex.ru>
2020-09-14 13:03:09 +0300

    140920

--- AtHome/script.js
+++ AtHome/script.js
@@ -1,35 +1,35 @@
-// Задание на 2 урок
-const fru = ['orange' , 'banana' , 'pineapple' , 'grapes']
-let fru2 = fru.slice();
-console.log(fru2)
+// // Задание на 2 урок
+// const fru = ['orange' , 'banana' , 'pineapple' , 'grapes']
+// let fru2 = fru.slice();
+// console.log(fru2)
 
 
-//____________________________________
+// //____________________________________
 
-let vege = ['Carrot' , 'Potato' , 'Tomato' , 'Cabbage']
-let getSting = vege.join()
-console.log(getSting)
+// let vege = ['Carrot' , 'Potato' , 'Tomato' , 'Cabbage']
+// let getSting = vege.join()
+// console.log(getSting)
 
-//____________________________________
-const sum = [1, 2, 3, 4, 5]
-sum2 = 0;
-for (let i = 0; i < sum.length; i++) {
-    sum2 += sum[i]
-}
-console.log(sum2)
+// //____________________________________
+// const sum = [1, 2, 3, 4, 5]
+// sum2 = 0;
+// for (let i = 0; i < sum.length; i++) {
+//     sum2 += sum[i]
+// }
+// console.log(sum2)
 
 
-//____________________________________
+// //____________________________________
 
-let lang = ['php' , 'php' , 'css' , 'css' , 'script' , 'script' , 'html' , 'html' , 'java']
-let lang2 = lang.slice()
-lang2.sort()
-for (let i = 0; i < lang2.length; i++) {
-    if (lang2[i] == lang2[i+1]) {
-        lang2.splice(0, lang2[i+1])
-    }
-}
-console.log(lang2)
+// // let lang = ['php' , 'php' , 'css' , 'css' , 'script' , 'script' , 'html' , 'html' , 'java']
+// // let lang2 = lang.slice()
+// // lang2.sort()
+// // for (let i = 0; i < lang2.length; i++) {
+// //     if (lang2[i] == lang2[i+1]) {
+// //         lang2.splice(0, lang2[i+1])
+// //     }
+// // }
+// // console.log(lang2)
 
 
 // let lang = ['php' , 'php' , 'css' , 'css' , 'script' , 'script' , 'html' , 'html' , 'java']
@@ -38,13 +38,64 @@ console.log(lang2)
 //             });
 // console.log(lang)
 
+// // //____________________________________
+// let aa = [1, 1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 7, 8]
+// function Getfiltr(a) {
+//   for (var b=1, i=1; b<aa.length; ++b) {
+//     if (aa[b] !== aa[b-1]) {
+//       aa[++i] = aa[b];
+//     }
+//   }
+//   aa.length = i; //Отличия var от let!!!!
+//   return aa;
 
-
-
-// var res = [];
-// newarr.sort();
-// for(let i = 0; i < newarr.length; i++){
-// if(newarr[i] != newarr[i+1]){
-// res.push(newarr[i]);
 // }
+// console.log(Getfiltr())
+
+
+// let arr = [1, 1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 7, 8]
+// uniqueArray = arr.filter(function(item, pos) {
+//     return arr.indexOf(item) == pos;
+// })
+// console.log(uniqueArray)
+// //____________________________________
+
+// const falsyArray = [NaN, 0, 7, false, -17, '', undefined, 99, null]
+//     for (let i=1; i<falsyArray.length; ++i) {
+//         if (Boolean(falsyArray[i]) == true) {
+//             console.log(falsyArray[i])
+//         }
+        
 // }
+
+
+// const falsyArray = [NaN, 0, 7, false, -17, '', undefined, 99, null]
+// function bouncer(arr) {
+//   return falsyArray.filter(Boolean);
+// }
+// bouncer(falsyArray);
+// console.log(bouncer())
+
+// //____________________________________
+
+let someNews = "Антон, Дима и Женя пришли на День рождения сразу, а Миша пришел позже"
+let anyNews = someNews.replace(/,/gi, ' ,')
+newBlock = anyNews.split(' ');
+newBlock2 = anyNews.split(' ');
+newBlock2.sort()
+
+newMas = newBlock.slice(6,10);
+newMas.splice(3, 1, 'пришли', ':')
+
+newMas2 = newBlock2.slice(1, 3)
+newMas2.reverse()
+
+newMas3 = newBlock2.slice(4, 7)
+newMas3JO = newMas3.join(', ')
+
+const FinalBlock = newMas.concat(newMas2, newMas3JO);
+
+newMasString = FinalBlock.join(' ');
+console.log (newMasString)
+
+// //____________________________________
