9aa18ba56b0fb29b0e3157ed0662d25357990f3c
NikitaKhadnevich <vadim-swich@yandex.ru>
2020-11-26 20:53:19 +0300

    [MASTER] Added 4 test JS work

---
+++ AtHome/MyFunc/4 Массивы/index.js
@@ -0,0 +1,140 @@
+l = (result) => {
+   console.log(result);
+}// 
+
+// Массивы
+// 1 .______________________________________________________________
+// let array = [];
+// function createArr(arr) {
+//    while (arr.length < 10) {
+//       arr.push(0)
+//    }
+//    arr[0] = 1;
+//    arr[arr.length] = 1
+//    return arr
+// }
+// const xxx = createArr(array);
+// l(xxx)
+
+// 2.______________________________________________________________
+// let array = [];
+// function changed(arr) {
+//    arr.push(0);
+//    arr.forEach(function (item, a) {
+//       arr[a];
+//       while (arr.length < 10) {
+//             arr.push(1);
+//             arr.push(0);
+//       }
+//    });
+//    return (arr)
+// }
+// let newarr = changed(array)
+// l(newarr)
+
+// 3.______________________________________________________________
+// let someArr = [];
+// function createSomearr(arr, counter) {
+//    counter = 1;
+//    while (arr.length < 10) {
+//       arr.push(counter)
+//       counter += 2
+//    }
+//    l(arr)
+// }
+// createSomearr(someArr)
+
+// 4.______________________________________________________________
+// let someArr = [];
+// function arithmetic(arr, x, counter, d) {
+//    counter = 0;
+//    d = 3;
+//    while (arr.length < 20) {
+//       arr.push(counter)
+//       counter += d
+//    }
+//    l(arr)
+// }
+// arithmetic(someArr, 1)
+// 5.______________________________________________________________
+
+// let someArr = [];
+// function createEven(arr, x, counter, d) {
+//    counter = 2;
+//    while (arr.length < 20) {
+//       arr.push(counter)
+//       counter += 2
+//    }
+//    l(arr)
+// }
+// l(createEven(someArr))
+
+// 6.______________________________________________________________
+
+// let someArr = [];
+// function createEven(arr, x, counter, d) {
+//    counter = 0;
+//    while (arr.length < 10) {
+//       arr.push(counter)
+//       counter += 3
+//    }
+//    l(arr.reverse())
+// }
+// l(createEven(someArr))
+
+// 7____________________________________________________________
+// let someArr = [];
+// function createFib(arr, counterPlus, counterMinus, n)  {
+//    arr.push(1, 2)
+//    counterPlus = arr[n];
+//    counterMinus = arr[n-1];
+//    while (arr.length < 10) {
+//       arr.push(counterPlus + counterMinus);
+//       ++n;
+//       counterPlus = arr[n];
+//       counterMinus = arr[n - 1];
+//     }
+//     l(arr)
+// }
+// createFib(someArr,0, 1, 1)
+//  8____________________________________________________________
+// let someArr = []
+// function randomNumb(arr) {
+//    while (arr.length < 10) {
+//       let x = Math.random()*10
+//       arr.push(Math.round(x))
+//    }
+//    l(arr)
+// }
+// randomNumb(someArr)
+//  9____________________________________________________________
+// let someArr = [];
+// function createSquare(arr, n) {
+//    n = 1;
+//    while (arr.length < 10) {
+//          arr.push(n*n)
+//          ++n
+//          }       
+//    l(arr)
+// }
+// createSquare(someArr)
+//  10 ____________________________________________________________
+// let someArr = [];
+// function randomNumb(arr, n) {
+//    while (arr.length < 6) {
+//       if (Number.isInteger(n/2) == false) {
+//          arr.push(1);
+//          ++n
+//       } 
+//       if (Number.isInteger(n / 2) == true)
+//          arr.push(n % 5);
+//          ++n
+//       }
+//    l(arr)
+// }
+// randomNumb(someArr, 1)
+
+
+
+
+
---
+++ AtHome/MyFunc/5 Польз функ/index.js
@@ -0,0 +1,51 @@
+l = (result) => {
+   console.log(result);
+}// 
+
+// .1 _________________________________________________________
+
+// let newStr = 'что где когда'
+
+// function ucfirst(string,arr) {
+//    arr = string.split(' ')
+//    arr.forEach(function (item, i)  {
+//       arr[i];
+//       var at = arr[i].charAt(0).toUpperCase();
+//       return arr[i] = at + arr[i].substr(1, arr[i].length - 1);
+//    });
+//    return arr  /* ВОЗВРАЩАЕМ РЕЗУЛЬТАТ  */
+// }
+
+// function newCall(string) {
+//    return ucfirst(string).join(' '); /* ВОЗВРАЩАЕМ РЕЗУЛЬТАТ ПРЕОБРАЗОВАНИЯ В СТРОКУ */
+//    }
+// let yyy = newCall(newStr)
+// l(yyy)
+
+// .2 _________________________________________________________
+// let someStr = 'var_text_hello'
+
+// function changeStr(str, arr) {
+//   arr = str.split('')
+
+//   arr.forEach(function (item, i) {
+//      if (arr[i] == "_") {
+//         arr[i+1] = arr[i + 1].toUpperCase()
+//      }  
+//   });
+
+//   return  arr.join('').replace(/[^a-zа-яё]/gi, '');
+// }
+
+// let xxx = changeStr(someStr)
+// l(xxx)
+
+// .3 _________________________________________________________
+
+
+
+
+
+
+
+
